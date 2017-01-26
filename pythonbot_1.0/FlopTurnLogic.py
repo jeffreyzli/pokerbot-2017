1a5600f97e92a9be3d0ca4eebd15541a485b8611
Brandon Baraban <brandonbaraban@users.noreply.github.com>
2017-01-25 22:19:03 -0500

    Add GameData and merge with existing code
    Basically rewrote everything to be clean and free of errors, easy to read, and now has access to GameData object with match stats.

--- pythonbot_1.0/FlopTurnLogic.py
+++ pythonbot_1.0/FlopTurnLogic.py
@@ -1,69 +1,82 @@
-from HandRankings import hand_win_odds
 
-
-def getaction(streetDict, potsize, numBoardCards, boardCards, numLastActions, lastActions, numMoves, moves, myHand, button):
+def action(game_data):
 
     # if button is True, you are dealer --> you act first pre-flop, then act second post-flop
 
-    #postFlop, preTurn
+    # post-flop, pre-turn
 
-    odds = hand_win_odds(myHand)
+    if game_data.discard:
+        if game_data.current_hand_strength <= 0.55:  # WE ARE GOING TO DISCARD
+            card_one = game_data.current_hand[0][0]
+            card_two = game_data.current_hand[1][0]
+
+            if card_one == 'T':
+                card_one = 10
+            elif card_one == 'J':
+                card_one = 11
+            elif card_one == 'Q':
+                card_one = 12
+            elif card_one == 'K':
+                card_one = 13
+            elif card_one == 'A':
+                card_one = 14
+            else:
+                card_one = int(card_one)
+
+            if card_two == 'T':
+                card_two = 10
+            elif card_two == 'J':
+                card_two = 11
+            elif card_two == 'Q':
+                card_two = 12
+            elif card_two == 'K':
+                card_two = 13
+            elif card_two == 'A':
+                card_two = 14
+            else:
+                card_one = int(card_one)
+
+            if card_one <= card_two:
+                return "DISCARD:" + game_data.current_hand[0] + "\n"
+            else:
+                return "DISCARD:" + game_data.current_hand[1] + "\n"
+        else:
+            game_data.discard = False
+
     aggressive = False
-    if odds > 0.55:     #Decided this value after playing around with 0.50, 0.53, 0.55, 0.57, 0.60, 0.70
+    if game_data.current_hand_strength > 0.55:  # Decided this value after playing around with 0.50, 0.53, 0.55,
+        # 0.57, 0.60, 0.70
         aggressive = True
-    betBool = False     #is Bet a legal move?
-    raiseBool = False   #is Raise a legal move?
 
     if aggressive:
-        for move in moves:
-            if "BET" in move:
-                betBool = True
-                raiseBool = False
-
-                colIndex = move.index(":")  # Parses the min and max bet
-                firstSlice = move[colIndex + 1:]
-                colIndex2 = firstSlice.index(":")
-                minBet = int(firstSlice[:colIndex2]) #int(move[colIndex + 1:colIndex2])
-                maxBet = int(firstSlice[colIndex2 + 1:]) #int(move[colIndex2 + 1:])
-            elif "RAISE" in move:
-                betBool = False
-                raiseBool = True
-
-                colIndex = move.index(":")  # Parses the min and max bet
-                firstSlice = move[colIndex + 1:]
-                colIndex2 = firstSlice.index(":")
-                minRaise = int(firstSlice[:colIndex2]) #int(move[colIndex + 1:colIndex2])
-                maxRaise = int(firstSlice[colIndex2 + 1:]) #int(move[colIndex2 + 1:])
-
-        if betBool and potsize <= 4:                                                #IF we have made it here CheckFold, and are now aggressive, bet big
-            betSize = (minBet + maxBet) / 2
-            return "BET:" + str(betSize)
-        elif raiseBool and potsize <= 4:
-            raiseSize = (minRaise + maxRaise) / 2
-            return "RAISE:" + str(raiseSize)
-
-        elif betBool and streetDict['3'] < 2:                                         #Ideally, should prevent more than 3-bet
-            betSize = min(potsize * 0.67, (minBet + maxBet) / 2)
-            return "BET:" + str(betSize)
-        elif raiseBool and streetDict['3'] < 2:                                     #Ideally, should prevent more than 3-bet
-            betSize = min(potsize * 0.67, (minRaise + maxRaise) / 2)
-            return "RAISE:" + str(betSize)
-        elif odds >= 0.573 and raiseBool and streetDict['3'] < 3:       #PFR to be implemented here. When Aggressive, we want to RAISE 70% of the time and CALL 30% of the time.
-            print("SUPER AGGRESSIVE")
-            betSize = min(potsize * 0.67, (minRaise + maxRaise) / 2)
-            return "RAISE:" + str(betSize)
+        limit = game_data.legal_action('BET')
+        if limit is not None:
+            if game_data.current_pot_size <= game_data.big_blind * 2:
+                bet = (limit[0] + limit[1]) / 2
+                return 'BET:' + str(bet)
+            else:
+                bet = min(game_data.current_pot_size * 5 / 3, (limit[0] + limit[1]) / 2)
+                return 'BET:' + str(bet)
         else:
-            return "CALL"
-
+            limit = game_data.legal_action('RAISE')
+            if limit is not None:
+                if game_data.button and not game_data.has_four_bet:
+                    bet = min(game_data.current_pot_size * 5 / 3, (limit[0] + limit[1]) / 2)
+                    return 'RAISE:' + str(bet)
+                elif not game_data.button and not game_data.has_three_bet:
+                    bet = min(game_data.current_pot_size * 5 / 3, (limit[0] + limit[1]) / 2)
+                    return 'RAISE:' + str(bet)
+                elif game_data.current_hand_strength >= 0.573 and not game_data.has_bet_aggressively:
+                    bet = min(game_data.current_pot_size * 5 / 3, (limit[0] + limit[1]) / 2)
+                    game_data.has_bet_aggressively = True
+                    return 'RAISE:' + str(bet)
+                # PFR to be implemented here. When Aggressive, we want to RAISE 70% of the time and CALL 30% of the
+                    # time.
+        return "CALL"
     else:
-        '''for move in moves:
-            if "CALL" in move:
-                return "CALL"'''
         return "CHECK"
-
-#Change bet sizes to max of 2/3rd of Pot Size and odds*(average of min and max)
-
-    #Discard Strategy Gen[1]:
-        #WHEN to discard
-        #WHAT to discard
-    #How to interface with discards in the engine
+# Change bet sizes to max of 2/3rd of Pot Size and odds*(average of min and max)
+# Discard Strategy Gen[1]:
+# WHEN to discard
+# WHAT to discard
+# How to interface with discards in the engine
---
+++ pythonbot_1.0/GameData.py
@@ -0,0 +1,330 @@
+import HandRankings as Hand
+
+
+class GameData:
+    def __init__(self, name, opponent_name, bb):
+        # match stats
+        self.name = name
+        self.opponent_name = opponent_name
+        self.num_hands = 0
+        self.num_wins = 0
+        self.num_flop = 0
+        self.big_blind = int(bb)
+        # self pre-flop stats
+        self.pfr = 0
+        self.vpip = 0
+        self.three_bet = 0
+        self.fold_big_bet = 0
+        # opponent pre-flop stats
+        self.opponent_pfr = 0
+        self.opponent_vpip = 0
+        self.opponent_three_bet = 0
+        self.opponent_fold_pfr = 0
+        self.opponent_fold_three_bet = 0
+        # self post-flop stats
+        self.aggression_factor = 0.0
+        self.showdown = 0
+        self.c_bet = 0
+        self.showdown_win = 0
+        self.double_barrel = 0
+        # opponent post-flop stats
+        self.opponent_c_bet = 0
+        self.opponent_fold_c_bet = 0
+        self.opponent_double_barrel = 0
+        # current hand stats
+        self.button = True
+        self.current_pot_size = 0
+        self.current_hand = []
+        self.current_hand_strength = 0.0
+        self.current_game_state = ''
+        self.board_cards = []
+        self.last_actions = []
+        self.current_legal_actions = []
+        self.has_called = False
+        self.opponent_has_called = False
+        self.has_two_bet = False
+        self.opponent_has_two_bet = False
+        self.has_three_bet = False
+        self.opponent_has_three_bet = False
+        self.has_four_bet = False
+        self.opponent_has_four_bet = False
+        self.street_dict = {'0': 0, '3': 0, '4': 0, '5': 0}
+        self.discard = False
+        self.has_five_bet = False
+        self.has_bet_aggressively = False
+
+    def new_hand(self, data_list):
+        self.num_hands += 1
+        self.button = data_list[2]
+        if "true" in self.button:
+            self.button = True
+        else:
+            self.button = False
+        self.current_hand = [data_list[3], data_list[4]]
+        self.current_hand_strength = Hand.hand_win_odds(self.current_hand)
+        self.current_game_state = 'PREFLOP'
+        self.board_cards = []
+        self.last_actions = []
+        self.current_legal_actions = []
+        self.street_dict = {'0': 0, '3': 0, '4': 0, '5': 0}
+        self.has_two_bet = False
+        self.opponent_has_two_bet = False
+        self.has_three_bet = False
+        self.opponent_has_three_bet = False
+        self.has_four_bet = False
+        self.opponent_has_four_bet = False
+        self.has_bet_aggressively = False
+
+    def get_action(self, data_list):
+        self.current_pot_size = int(data_list[1])
+
+        num_board_cards = int(data_list[2])
+        self.street_dict[str(num_board_cards)] += 1
+        if self.current_game_state == 'PREFLOP':
+            if self.street_dict['3'] > 0 and self.street_dict['4'] == 0:
+                self.has_two_bet = False
+                self.opponent_has_two_bet = False
+                self.has_three_bet = False
+                self.opponent_has_three_bet = False
+                self.has_four_bet = False
+                self.opponent_has_four_bet = False
+                self.has_bet_aggressively = False
+                self.current_game_state = 'FLOPTURN'
+                self.num_flop += 1
+        elif self.current_game_state == 'FLOPTURN':
+            if self.street_dict['4'] > 0 and self.street_dict['5'] == 0:
+                self.has_two_bet = False
+                self.opponent_has_two_bet = False
+                self.has_three_bet = False
+                self.opponent_has_three_bet = False
+                self.has_four_bet = False
+                self.opponent_has_four_bet = False
+                self.has_bet_aggressively = False
+                self.current_game_state = 'TURNRIVER'
+        elif self.current_game_state == 'TURNRIVER':
+            if self.street_dict['5'] > 0:
+                self.has_two_bet = False
+                self.opponent_has_two_bet = False
+                self.has_three_bet = False
+                self.opponent_has_three_bet = False
+                self.has_four_bet = False
+                self.opponent_has_four_bet = False
+                self.has_bet_aggressively = False
+                self.current_game_state = 'POSTFLOP'
+        for i in range(num_board_cards):
+            self.board_cards.append(data_list[3 + i])
+
+        index = 3 + num_board_cards
+        num_last_actions = int(data_list[index])
+        index += 1
+        current_last_actions = []
+        for i in range(num_last_actions):
+            current_last_actions.append(data_list[index + i])
+
+        if self.discard:
+            for action in current_last_actions:
+                if 'DISCARD' in action:
+                    old_card = action[8:10]
+                    new_card = action[11:13]
+                    self.current_hand[self.current_hand.index(old_card)] = new_card
+                    self.current_hand_strength = Hand.hand_win_odds(self.current_hand)
+                    self.discard = False
+                    break
+
+        if self.current_game_state == 'PREFLOP':
+            if self.current_pot_size == 4:
+                if self.button:
+                    self.vpip += 1
+                    self.has_called = True
+                else:
+                    self.opponent_vpip += 1
+                    self.opponent_has_called = True
+            else:
+                for action in current_last_actions:
+                    if 'RAISE' in action:
+                        round_num = self.street_dict['0']
+                        if round_num == 1:
+                            self.opponent_pfr += 1
+                            self.opponent_vpip += 1
+                            self.opponent_has_two_bet = True
+                        elif round_num == 2:
+                            if self.button:
+                                if self.name in action:
+                                    self.pfr += 1
+                                    self.vpip += 1
+                                    self.has_two_bet = True
+                                else:
+                                    self.opponent_pfr += 1
+                                    self.opponent_vpip += 1
+                                    self.opponent_has_three_bet = True
+                            else:
+                                if self.name in action:
+                                    self.pfr += 1
+                                    self.vpip += 1
+                                    self.has_three_bet = True
+                                else:
+                                    self.opponent_pfr += 1
+                                    self.opponent_vpip += 1
+                                    self.opponent_has_four_bet = True
+                        elif round_num == 3:
+                            if self.name in action:
+                                self.pfr += 1
+                                self.vpip += 1
+                    elif 'CALL' in action:
+                        if self.name in action:
+                            self.vpip += 1
+                        else:
+                            self.opponent_vpip += 1
+        elif self.current_game_state == 'FLOPTURN':
+            round_num = self.street_dict['3']
+            if round_num == 1:
+                self.discard = True
+            elif round_num == 2:
+                for action in current_last_actions:
+                    if 'BET' in action:
+                        self.opponent_c_bet += 1
+                        break
+            elif round_num == 3:
+                for action in current_last_actions:
+                    if 'BET' in action:
+                        if self.name in action:
+                            self.c_bet += 1
+                        else:
+                            self.opponent_c_bet += 1
+                    elif 'RAISE' in action:
+                        if self.name in action:
+                            self.has_two_bet = True
+                        else:
+                            if self.button:
+                                self.opponent_has_three_bet = True
+                            else:
+                                self.opponent_has_two_bet = True
+            elif round_num == 4:
+                for action in current_last_actions:
+                    if 'RAISE' in action:
+                        if self.name in action:
+                            if self.button:
+                                self.has_four_bet = True
+                            else:
+                                self.has_three_bet = True
+                        break
+        elif self.current_game_state == 'TURNRIVER':
+            round_num = self.street_dict['4']
+            if round_num == 1:
+                self.discard = True
+                for action in current_last_actions:
+                    if 'BET' in action:
+                        if self.name in action:
+                            self.c_bet += 1
+                        else:
+                            self.opponent_c_bet += 1
+                        break
+            elif round_num == 2:
+                for action in current_last_actions:
+                    if 'BET' in action:
+                        self.opponent_c_bet += 1
+                        break
+            elif round_num == 3:
+                for action in current_last_actions:
+                    if 'BET' in action:
+                        if self.name in action:
+                            self.c_bet += 1
+                        else:
+                            self.opponent_c_bet += 1
+                    elif 'RAISE' in action:
+                        if self.name in action:
+                            self.has_two_bet = True
+                        else:
+                            if self.button:
+                                self.opponent_has_three_bet = True
+                            else:
+                                self.opponent_has_two_bet = True
+            elif round_num == 4:
+                for action in current_last_actions:
+                    if 'RAISE' in action:
+                        if self.name in action:
+                            if self.button:
+                                self.has_four_bet = True
+                            else:
+                                self.has_three_bet = True
+                        break
+        elif self.current_game_state == 'POSTRIVER':
+            round_num = self.street_dict['5']
+            if round_num == 1:
+                for action in current_last_actions:
+                    if 'BET' in action:
+                        if self.name in action:
+                            self.double_barrel += 1
+                        else:
+                            self.opponent_double_barrel += 1
+                        break
+
+        index += num_last_actions
+        num_legal_actions = int(data_list[index])
+        index += 1
+        self.current_legal_actions = []
+        for i in range(num_legal_actions):
+            self.current_legal_actions.append(data_list[index + i])
+
+    def legal_action(self, action):
+        for legal_action in self.current_legal_actions:
+            if action in legal_action:
+                if action == 'BET' or action == 'RAISE':
+                    index = legal_action.index(':') + 1
+                    sub = legal_action[index:]
+                    index = sub.index(':')
+                    return [int(sub[:index]), int(sub[index+1:])]
+                else:
+                    return True
+        return None
+
+    def hand_over(self, data_list):
+        num_board_cards = data_list[3]
+        index = 4+num_board_cards
+        num_last_actions = data_list[index]
+        current_last_actions = []
+        for i in range(num_last_actions):
+            current_last_actions.append(data_list[index+i])
+        if self.current_game_state == 'PREFLOP':
+            for action in current_last_actions:
+                if 'FOLD' in action and self.opponent_name in action:
+                    if self.button:
+                        for last_action in self.last_actions[-1]:
+                            if 'RAISE' in last_action and self.name in last_action:
+                                self.opponent_fold_pfr += 1
+                                if self.has_three_bet and not self.has_four_bet:
+                                    self.opponent_fold_three_bet += 1
+                        self.num_wins += 1
+                    else:
+                        for last_action in current_last_actions:
+                            if 'RAISE' in last_action and self.name in last_action:
+                                self.opponent_fold_pfr += 1
+                                if self.has_three_bet and not self.has_four_bet:
+                                    self.opponent_fold_three_bet += 1
+                        self.num_wins += 1
+        elif self.current_game_state == 'FLOPTURN':
+            for action in current_last_actions:
+                if self.button:
+                    if 'FOLD' in action and self.opponent_name in action:
+                        for last_action in self.last_actions[-1]:
+                            if 'BET' in last_action and self.name in last_action:
+                                self.opponent_fold_c_bet += 1
+                    self.num_wins += 1
+                else:
+                    if 'FOLD' in action and self.opponent_name in action:
+                        for last_action in current_last_actions:
+                            if 'BET' in last_action and self.name in last_action:
+                                self.opponent_fold_c_bet += 1
+                    self.num_wins += 1
+
+        elif self.current_game_state == 'POSTRIVER':
+            for action in current_last_actions:
+                if 'WIN' in action:
+                    if self.name in action:
+                        self.num_wins += 1
+                    for last_action in current_last_actions:
+                        if 'SHOW' in last_action:
+                            self.showdown += 1
+                            self.showdown_win += 1
+                            break
+                    break
