cfe438d26250f766a2fe5ab9b36d7a96d0157738
Brandon Baraban <brandonbaraban@users.noreply.github.com>
2017-01-27 01:30:25 -0500

    Add new bot with new bet/fold logic
    New logic based on maximizing gains and minimizing losses based on pocket and hands as game progesses.

---
+++ BetFoldBot/BetFoldLogic.py
@@ -0,0 +1,74 @@
+from DiscardLogicSimple import discard_logic_post_flop, discard_logic_post_turn
+
+
+def pre_flop_hand_eval(my_hand):
+    hand_values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
+                   'T': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    card_one_value = hand_values[my_hand[0][0]]
+    card_one_suit = my_hand[0][1]
+    card_two_value = hand_values[my_hand[1][0]]
+    card_two_suit = my_hand[1][1]
+
+    if card_one_value == card_two_value:
+        if card_one_value > 4:
+            return True
+        return False
+    same_suit = card_one_suit is card_two_suit
+    card_min_value = min(card_one_value, card_two_value)
+    card_max_value = max(card_one_value, card_two_value)
+    if card_max_value == 14:
+        return True
+    elif card_max_value == 13:
+        return card_min_value > 5 or same_suit and card_min_value > 2
+    elif card_max_value == 12:
+        return card_min_value > 8 or same_suit and card_min_value > 5
+    elif card_max_value == 11:
+        return card_min_value > 9 or same_suit and card_min_value > 7
+
+
+def action(game_data):
+
+    if game_data.current_game_state is 'PREFLOP':
+        if pre_flop_hand_eval(game_data.current_hand):
+            limits = game_data.legal_action('RAISE')
+            if limits is not None:
+                return 'RAISE:' + str(limits[1])
+            return 'CALL'
+        elif game_data.current_hand_strength > 0.55:
+            limits = game_data.legal_action('CALL')
+            if limits is not None:
+                return 'CALL'
+        limits = game_data.legal_action('CHECK')
+        if limits:
+            return 'CHECK'
+        return 'FOLD'
+    if game_data.discard:
+        if game_data.current_game_state is 'FLOPTURN':
+            discard, card = discard_logic_post_flop(game_data.current_hand, game_data.board_cards)
+            if discard:
+                game_data.discarded_card = card
+                return 'DISCARD:' + card
+        elif game_data.current_game_state is 'TURNRIVER':
+            discard, card = discard_logic_post_turn(game_data.current_hand, game_data.board_cards,
+                                                    game_data.discarded_card)
+            if discard:
+                return 'DISCARD:' + card
+        game_data.discard = False
+    if 'High Card' in game_data.hand_class or 'Pair' in game_data.hand_class:
+        limits = game_data.legal_action('CHECK')
+        if limits:
+            return 'CHECK'
+        else:
+            return 'FOLD'
+    else:
+        if game_data.current_game_state is 'POSTRIVER':
+            limits = game_data.legal_action('BET')
+            if limits is not None:
+                return 'BET:' + str(limits[1])
+            limits = game_data.legal_action('RAISE')
+            if limits is not None:
+                return 'RAISE:' + str(limits[1])
+        limits = game_data.legal_action('CALL')
+        if limits is not None:
+            return 'CALL'
+        return 'CHECK'
---
+++ BetFoldBot/DiscardLogicSimple.py
@@ -0,0 +1,106 @@
+# post turn: IF 4 boardCards are the same suit AND IF both our cards are that suit --> discard lower one
+# post turn: IF 4 boardCards are the same suit AND
+# IF only one of our cards are that suit --> discard the non-suited hand
+# post turn: IF 4 boardCards are the same suit AND IF we have no cards of that suit --> discard [LOW] card
+# post turn: IF 3 boardCards are the same suit AND IF both our cards are that suit --> no discard
+# post turn: IF 3 boardCards are the same suit AND
+# IF only one of our cards are that suit --> discard the non-suited hand, IF no pairs anywhere
+
+# if [A] [A] --> no discard
+# elif [A] [B] with flop of [A] [C] [B] --> no discard
+# elif [Low] [High] with flop of [Low] [B] [C] --> discard High
+# else discard Low
+
+
+def discard_logic_post_flop(my_hand, board_cards):
+    cards_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10,
+                  'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    val_one = cards_dict[my_hand[0][0]]
+    val_two = cards_dict[my_hand[1][0]]
+
+    # if [A] [A] --> no discard
+    if val_one == val_two:
+        return False, None
+    # elif [A] [B] with flop of [A] [C] [B] --> no discard
+    elif val_one in board_cards and val_two in board_cards:
+        return False, None
+    # elif [Low] [High] with flop of [Low] [B] [C] --> discard High
+    elif min(val_one, val_two) in board_cards:
+        if max(val_one, val_two) == val_one:
+            return True, my_hand[0]
+        else:
+            return True, my_hand[1]
+    # else discard [Low]
+    else:
+        if min(val_one, val_two) == val_one:
+            return True, my_hand[0]
+        if min(val_one, val_two) == val_two:
+            return True, my_hand[1]
+
+            # return (True, discard) OR return (False, None)
+
+
+def discard_logic_post_turn(my_hand, board_cards, discarded_card=None):
+    if discarded_card is None:
+        pass
+    cards_dict = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, 'T': 10,
+                  'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    suit_dict = {'h': 0, 'd': 0, 's': 0, 'c': 0}
+
+    val_one = cards_dict[my_hand[0][0]]
+    val_two = cards_dict[my_hand[1][0]]
+    suit = None
+    hand_count = 0
+
+    for board_card in board_cards:
+        suit_dict[board_card[1]] += 1
+
+    # postTurn: IF 4 boardCards are the same suit AND
+    if 4 in suit_dict.values():
+        for pair in suit_dict.items():
+            if pair[1] == 4:
+                suit = pair[0]
+                break
+        for card in my_hand:
+            if card[1] == suit:
+                hand_count += 1
+
+        # IF only one of our cards are that suit --> discard the non-suited hand
+        if hand_count == 1:
+            for card in my_hand:
+                if card[1] is not suit:
+                    return True, card
+
+        # ELSE we have no cards of that suit OR we have both cards of that suit --> discard [LOW] card
+        else:
+            if min(val_one, val_two) == val_one:
+                return True, my_hand[0]
+            if min(val_one, val_two) == val_two:
+                return True, my_hand[1]
+
+    # postTurn: IF 3 boardCards are the same suit AND
+    elif 3 in suit_dict.values():
+        for pair in suit_dict.items():
+            if pair[1] == 3:
+                suit = pair[0]
+                break
+        for card in my_hand:
+            if card[1] == suit:
+                hand_count += 1
+
+        # IF both our cards are that suit --> no discard
+        if hand_count == 2:
+            return False, None
+
+        # IF only one of our cards are that suit --> discard the non-suited hand, IF no pairs anywhere
+        elif hand_count == 1:
+            if val_one == val_two or val_one in board_cards or val_two in board_cards:
+                return False, None
+            else:
+                for card in my_hand:
+                    if card[1] is not suit:
+                        return True, card
+
+        # IF no cards are that suit --> continue
+    return discard_logic_post_flop(my_hand, board_cards)
+    # return (True, discard) OR return (False, None)
